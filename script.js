1af0c74a13e62f7c73454afe34411a4908a933df
theflyingfire <64284477+theflyingfire@users.noreply.github.com>
2021-04-09 12:52:14 -0700

    v0.1.0

--- script.js
+++ script.js
@@ -10,3 +10,14 @@ var height = window.screen.availHeight;
 if(width<height){
 	//alert("portrait mode");
 }
+
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('sw.js', {scope: '.'})
+  .then((reg) => {
+    // registration worked
+    console.log('Registration succeeded. Scope is ' + reg.scope);
+  }).catch((error) => {
+    // registration failed
+    console.log('Registration failed with ' + error);
+  });
+}
