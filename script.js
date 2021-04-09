96e494965b8d755018feed383100ec1ad42753a8
theflyingfire <64284477+theflyingfire@users.noreply.github.com>
2021-04-09 12:43:22 -0700

    init

---
+++ script.js
@@ -0,0 +1,12 @@
+//alert("testing");
+
+
+var width = window.screen.availWidth;
+var height = window.screen.availHeight;
+
+//alert(width);
+//alert(height);
+
+if(width<height){
+	//alert("portrait mode");
+}
