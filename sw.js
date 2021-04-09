2b43bdd514f535f2dd52275f43694f6e5ef9b11b
theflyingfire <64284477+theflyingfire@users.noreply.github.com>
2021-04-09 13:29:25 -0700

    v0.2.2

--- sw.js
+++ sw.js
@@ -21,8 +21,8 @@ self.addEventListener('fetch', (event) => {
 
         return response;
       }).catch(() => {
-        return caches.match('./fallback.html');
-      })
+        return caches.match('fallback.html')
+      });
     });
   );
 });
