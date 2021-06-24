a4e3584e229b6f29490a6c90636afbbdfc4357e2
theflyingfire <64284477+theflyingfire@users.noreply.github.com>
2021-06-23 20:10:19 -0700

    v1.1.8

--- script.js
+++ script.js
@@ -35,19 +35,6 @@ window.addEventListener('beforeinstallprompt', (e) => {
   //addBtn.style.display = 'block';
   
 
-    // Show the prompt
-    deferredPrompt.prompt();
-    // Wait for the user to respond to the prompt
-    deferredPrompt.userChoice.then((choiceResult) => {
-        if (choiceResult.outcome === 'accepted') {
-          console.log('User accepted the A2HS prompt');
-        } else {
-          console.log('User dismissed the A2HS prompt');
-        }
-        deferredPrompt = null;
-    });
-  
-
 
   addBtn.addEventListener('click', (e) => {
     // hide our user interface that shows our A2HS button
@@ -64,4 +51,6 @@ window.addEventListener('beforeinstallprompt', (e) => {
         deferredPrompt = null;
       });
   });
+  
+  addBtn.click();
 });
